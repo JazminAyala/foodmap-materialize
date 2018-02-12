f53b65d5df0bcfa55273c63c75e6b14f4e02b178
JazminAyala <tarin_ayala@hotmail.com>
2018-02-12 01:57:23 -0600

    primer commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+/*   vista splasch  */
+$(document).ready(function() {
+    setTimeout(function() {
+        $(".content1").fadeOut(1500);
+    },3000);
+	
+	setTimeout(function() {
+        $(".content2").fadeIn(1500);
+    },6000);
+   
+});
+
+$('.modal').modal();
+
+
+$
---
+++ js/data.js
@@ -0,0 +1,38 @@
+var restaurants = [
+
+    {
+       name : "Becco-Four Seasons",
+       comida : "Italiana",
+       direction : "Paseo de la Reforma 500-Reforma",
+       description : "Es una antología en forma de la cocina italiana y mediterránea en el Nuevo Continente.",
+       phone : 55555555,
+    },
+    {
+        name : "Fiebre de Malta",
+        comida : "Internacional",
+        direction : "Rio Lerma 156 - Reforma",
+        description : "De su variada carta de presentaciones se destacan el fish & chips, los camarones popcorn y los tacos.",
+        phone : 55555555,
+     },
+     {
+        name : "El Diez",
+        comida : "Argentina",
+        direction : "Calle París 32 - Reforma",
+        description : "El menú es variado y todos los platos son caseros, lo cual favorece un ambiente relajado familiar.",
+        phone : 55555555,
+     },
+     {
+        name : "Gino´s",
+        comida : "Italiana",
+        direction : "Av. Paseo de la Reforma 316",
+        description : "Una versión casera de la gastronomía europea, como la clásica sopa de cebolla o la sopa de tomate.",
+        phone : 55555555,
+     },
+     {
+        name : "La Destilería s",
+        comida : "Mexicana",
+        direction : "Reforma 222 (2o Piso) - Reforma",
+        description : "Un restaurante ideal para disfrutar lo mejor de la cocina nacional, y acorde a cualquier ocasión.",
+        phone : 55555555,
+     },
+ ];
